dd3425bc4d2a6df620bf6e346dc1a717e33993eb
zabu <mcosutreemmanuel37@gmail.com>
2026-04-22 18:41:48 +0300

    bb

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
-self.addEventListener("install", (e) => {
-    console.log("Service Worker installed");
-  });
-  
-  self.addEventListener("fetch", (e) => {});
+const CACHE_NAME = "farm-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
